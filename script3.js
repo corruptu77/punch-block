4aab6c698a0b85277e8f21b4fa489d53312241be
Corruptu77 <corruptu77@gmail.com>
2026-06-08 17:28:16 +0000

    fuck

--- script3.js
+++ script3.js
@@ -34,18 +34,39 @@ let blockData = {
   velocityY: [],
   spin: []
 }
-var amount = 25;
+function spawnBlocks() {
+blockData = {
+  locationX: [],
+  locationY: [],
+  rotation: [],
+  velocityX: [],
+  velocityY: [],
+  spin: []
+}
+  for (var i = 0; i < amount+1; i ++) {
+    blockData.locationX.push(Math.floor(Math.random()*canvasX));
+    blockData.locationY.push(Math.floor(Math.random()*canvasY));
+    blockData.rotation.push(45);
+    blockData.velocityX.push(0);
+    blockData.velocityY.push(0);
+    blockData.spin.push(0);
+  }
+}
+
+var amount = 10;
 var gravityX = 0;
 var gravityY = 0.5;
 var bounciness = 0.7;
 var friction = 0.98;
-for (var i = 0; i < amount+1; i ++) {
-  blockData.locationX.push(Math.floor(Math.random()*canvasX));
-  blockData.locationY.push(Math.floor(Math.random()*canvasY));
-  blockData.rotation.push(45);
-  blockData.velocityX.push(0);
-  blockData.velocityY.push(0);
-  blockData.spin.push(0);
+var hitStrength = 1;
+var punchRadius = 25;
+var randomness = 2.5;
+var isHighRate = false;
+
+spawnBlocks();
+
+function simplyfyRotation(index) {
+  blockData.rotation[index] = blockData.rotation[index]%360;
 }
 
 console.log(blockData);
@@ -55,7 +76,10 @@ function drawCursor() {
   stroke(0);
   strokeWeight(4);
   noFill();
-  ellipse(mouseX,mouseY,50,50);
+  ellipse(mouseX,mouseY,punchRadius*2,punchRadius*2);
+}
+function pickValueFromRandom() {
+  return((Math.random()-0.5)*randomness);
 }
 function drawBlock(index) {
   strokeWeight(4);
@@ -79,9 +103,9 @@ function drawBlock(index) {
   endShape(CLOSE);
 }
 function doPunch(index) {
-  var punchX = -(mouseX - blockData.locationX[index]);
-  var punchY = -(mouseY - blockData.locationY[index]);
-  var punchSpin= (punchX+punchY)/2;
+  var punchX = -((mouseX - blockData.locationX[index])*hitStrength);
+  var punchY = -((mouseY - blockData.locationY[index])*hitStrength);
+  var punchSpin= -((punchX+punchY)/2);
   blockData.velocityX[index] += punchX;
   blockData.velocityY[index] += punchY;
   blockData.spin[index] += punchSpin;
@@ -96,26 +120,37 @@ function doWalls(index) {
   if (blockData.locationX[index]<10) {
     blockData.locationX[index] = 10;
     if (blockData.velocityX[index]<0) {
-      blockData.velocityX[index] = (-blockData.velocityX[index])*bounciness;
+      blockData.velocityX[index] = ((-blockData.velocityX[index]+blockData.spin[index])+(pickValueFromRandom()*blockData.velocityX[index]))*bounciness;
     }
+    spinOnWallBounce(index,"x");
   }
   if (blockData.locationY[index]<10) {
     blockData.locationY[index] = 10;
     if (blockData.velocityY[index]<0) {
-      blockData.velocityY[index] = (-blockData.velocityY[index])*bounciness;
+      blockData.velocityY[index] = ((-blockData.velocityY[index]+blockData.spin[index])+(pickValueFromRandom()*blockData.velocityY[index]))*bounciness;
     }
+    spinOnWallBounce(index,"y");
   }
   if (blockData.locationX[index]>canvasX-10) {
     blockData.locationX[index] = canvasX-10;
     if (blockData.velocityX[index]>0) {
-      blockData.velocityX[index]=(-blockData.velocityX[index])*bounciness;
+      blockData.velocityX[index]=((-blockData.velocityX[index])+(pickValueFromRandom()*blockData.velocityY[index]))*bounciness;
     }
+    spinOnWallBounce(index,"x");
   }
   if (blockData.locationY[index]>canvasY-10) {
     blockData.locationY[index] = canvasY-10;
     if (blockData.velocityY[index]>0) {
-      blockData.velocityY[index]=(-blockData.velocityY[index])*bounciness;
+      blockData.velocityY[index]=((-blockData.velocityY[index]+blockData.spin[index])+(pickValueFromRandom()*blockData.velocityX[index]))*bounciness;
     }
+    spinOnWallBounce(index,"y");
+  }
+}
+function spinOnWallBounce(index,d) {
+  if (d === "x") {
+    blockData.spin[index] += -(blockData.velocityY[index]*2+blockData.spin[index])*bounciness+(pickValueFromRandom()*blockData.velocityY[index]);
+  } else if (d === "y") {
+    blockData.spin[index] += -(blockData.velocityX[index]+blockData.spin[index])*bounciness+(pickValueFromRandom()*blockData.velocityX[index]);
   }
 }
 function calculateVelocity(index) {
@@ -127,6 +162,7 @@ function calculatePosition(index) {
   blockData.locationX[index] += blockData.velocityX[index];
   blockData.locationY[index] += blockData.velocityY[index];
   blockData.rotation[index] += blockData.spin[index];
+  simplyfyRotation(index);
 }
 function calculateVector(index) {
   var rotationalPoint = blockData.rotation[index]
@@ -141,7 +177,7 @@ function draw() {
   drawCursor();
   for (var i = 0; i < amount; i ++) {
     drawBlock(i);
-    if (dist(mouseX, mouseY, blockData.locationX[i], blockData.locationY[i]) < 25&&mouseIsPressed) {
+    if (dist(mouseX, mouseY, blockData.locationX[i], blockData.locationY[i]) < punchRadius&&mouseIsPressed) {
   doPunch(i);
 }
   }
@@ -172,22 +208,15 @@ document.getElementById("bouncinessSlider").addEventListener("input",function() 
 document.getElementById("amountSlider").addEventListener("input",function() {
   amount = Number(this.value);
   document.getElementById("an").textContent = amount;
-  blockData = {
-    locationX: [],
-    locationY: [],
-    rotation: [],
-    velocityX: [],
-    velocityY: [],
-    spin: []
-  }
-  for (var i = 0; i < amount+1; i ++) {
-    blockData.locationX.push(Math.floor(Math.random()*canvasX));
-    blockData.locationY.push(Math.floor(Math.random()*canvasY));
-    blockData.rotation.push(45);
-    blockData.velocityX.push(0);
-    blockData.velocityY.push(0);
-    blockData.spin.push(0);
-  }
+  spawnBlocks();
+});
+document.getElementById("punchRadius").addEventListener("input",function() {
+  punchRadius = document.getElementById("punchRadius").value;
+  document.getElementById("pr").textContent = punchRadius;
+});
+document.getElementById("randomnessSlider").addEventListener("input", function() {
+  randomness = document.getElementById("randomnessSlider").value;
+  document.getElementById("rn").textContent = randomness;
 });
 document.getElementById("start").addEventListener("click",function() {
   loop();
@@ -218,4 +247,62 @@ document.getElementById("resetRotation").addEventListener("click",function() {
     blockData.rotation[i] = 45;
     blockData.spin[i] = 0;
   }
+});
+document.getElementById("highRateValues").addEventListener("click",function() {
+  if (!(isHighRate)) {
+    if (!confirm("Are you sure you want to set values to high rate? Your device may not handle it if you aren't careful. If the simulation crashes, click 'reset values', then 'stop the script' then 'start the script' ")) {
+      return;
+    }
+  document.getElementById("gravitySliderX").max= 100;
+  document.getElementById("gravitySliderX").min= -100;
+  document.getElementById("gravitySliderY").max= 100;
+  document.getElementById("gravitySliderY").min= -100;
+  document.getElementById("frictionSlider").max= 10;
+  document.getElementById("bouncinessSlider").max= 10;
+  document.getElementById("punchSlider").max= 10;
+  document.getElementById("amountSlider").max= 100000;
+  document.getElementById("punchRadius").max= 500;
+  document.getElementById("randomnessSlider").max= 100;
+  isHighRate = true;
+} else if (isHighRate) {
+  document.getElementById("gravitySliderX").max=3;
+  document.getElementById("gravitySliderX").min=-3;
+  document.getElementById("gravitySliderY").max=3;
+  document.getElementById("gravitySliderY").min=-3;
+  document.getElementById("frictionSlider").max= 2;
+  document.getElementById("bouncinessSlider").max= 1;
+  document.getElementById("punchSlider").max= 2;
+  document.getElementById("amountSlider").max= 1000;
+  document.getElementById("punchRadius").max= 100;
+  document.getElementById("randomnessSlider").max= 10;
+  isHighRate = false;
+}
+});
+document.getElementById("resetValues").addEventListener("click",function() {
+  gravityX = 0;
+  gravityY = 0.5;
+  bounciness = 0.7;
+  friction = 0.98;
+  hitStrength = 1;
+  amount = 10;
+  punchRadius = 25;
+  randomness = 2.5
+  document.getElementById("gnX").textContent = gravityX;
+  document.getElementById("gnY").textContent = gravityY;
+  document.getElementById("fn").textContent = friction;
+  document.getElementById("bn").textContent = bounciness;
+  document.getElementById("ps").textContent = hitStrength;
+  document.getElementById("an").textContent = amount;
+  document.getElementById("pr").textContent = punchRadius;
+  document.getElementById("rn").textContent = randomness;
+  
+  document.getElementById("gravitySliderX").value = gravityX;
+  document.getElementById("gravitySliderY").value = gravityY;
+  document.getElementById("frictionSlider").value = friction;
+  document.getElementById("bouncinessSlider").value = bounciness;
+  document.getElementById("punchSlider").value = hitStrength;
+  document.getElementById("amountSlider").value = amount;
+  document.getElementById("punchRadius").value = punchRadius;
+  document.getElementById("randomnessSlider").value = randomness;
+  spawnBlocks();
 });
